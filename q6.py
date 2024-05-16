abd4cd9b535364b0c78e65f0bc408204b23d5eff
AMKiasat <kiasatamirm@gmail.com>
2024-05-16 23:38:52 +0330

    Adding test and plotting to q6

--- q6.py
+++ q6.py
@@ -8,10 +8,10 @@ def f(x):
 
 
 def gaussian(x, c, v):
-    return torch.exp(-((x - c) ** 2) / (2 * v ** 2))
+    return np.exp(-((x - c) ** 2) / (2 * v ** 2))
 
 
-def train_RBF(x, y, center_num=10):
+def train_RBF(x, y, center_num):
     d = []
 
     for i in range(len(x)):
@@ -26,20 +26,33 @@ def train_RBF(x, y, center_num=10):
     for i in range(len(x)):
         for j in range(len(centers)):
             g_distances[i, j] = gaussian(x[i], centers[j], variance)
-    return np.dot(np.linalg.pinv(g_distances), y)
+    wights = np.dot(np.linalg.pinv(g_distances), y)
+
+    return wights, centers, variance
+
+
+def test_RBF(x, wights, centers, variance):
+    g_distances = np.zeros((len(x), len(centers)))
+    for i in range(len(x)):
+        for j in range(len(centers)):
+            g_distances[i, j] = gaussian(x[i], centers[j], variance)
+    return np.dot(g_distances, wights)
 
 
 if __name__ == '__main__':
     x = np.linspace(0, 10, 500)
     y = f(x)
+    c_num = 12
 
-    train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size=0.3, random_state=43)
+    train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size=0.3)
 
-    train_RBF(train_data, train_labels, center_num=15)
+    w, c, v = train_RBF(train_data, train_labels, c_num)
+    predictions = test_RBF(test_data, w, c, v)
 
-    # plt.plot(x, y, label='data', color='black')
-    #
-    # plt.legend()
-    # plt.xlabel('x')
-    # plt.ylabel('y')
-    # plt.show()
+    plt.scatter(test_data, predictions, label='Predicted', color='red', marker='o')
+    plt.plot(x, y, label='data', color='black')
+
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
