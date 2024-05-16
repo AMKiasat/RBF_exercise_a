bd0bf7998cd02fd64318ba8eff81b8337ebe784a
AMKiasat <kiasatamirm@gmail.com>
2024-05-16 23:19:10 +0330

    Adding train to q6

--- q6.py
+++ q6.py
@@ -10,15 +10,32 @@ def f(x):
 def gaussian(x, c, v):
     return torch.exp(-((x - c) ** 2) / (2 * v ** 2))
 
-# def train_RBF()
+
+def train_RBF(x, y, center_num=10):
+    d = []
+
+    for i in range(len(x)):
+        for j in range(i + 1, len(x)):
+            d.append(np.linalg.norm(x[i] - x[j]))
+    variance = np.sort(d)[int(len(x) * (len(x) / 2) / 2)]
+    # print(variance)
+
+    centers = np.random.choice(x, size=center_num)
+    g_distances = np.zeros((len(x), center_num))
+
+    for i in range(len(x)):
+        for j in range(len(centers)):
+            g_distances[i, j] = gaussian(x[i], centers[j], variance)
+    return np.dot(np.linalg.pinv(g_distances), y)
 
 
 if __name__ == '__main__':
     x = np.linspace(0, 10, 500)
-    print(x.shape)
     y = f(x)
 
-    train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size=0.9, random_state=43)
+    train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size=0.3, random_state=43)
+
+    train_RBF(train_data, train_labels, center_num=15)
 
     # plt.plot(x, y, label='data', color='black')
     #
